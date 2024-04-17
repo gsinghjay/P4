770b94ef788226cea106175a7edfbea9cad40e19
Jay Singh <54427246+gsinghjay@users.noreply.github.com>
2024-04-16 21:19:26 -0400

    added php code to get current page and add active tag

--- includes/nav.php
+++ includes/nav.php
@@ -1,3 +1,5 @@
+<?php $currentPage = basename($_SERVER['SCRIPT_NAME']); ?> 
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container">
         <a class="navbar-brand" href="/">J&K Sea Moss</a>
@@ -7,33 +9,34 @@
         <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#"><i class="fas fa-home"></i> Home</a>
+            <a class="nav-link <?php if($currentPage == 'index.php'){echo 'active';} ?>" aria-current="page" href="index.php"><i class="fas fa-home"></i> Home</a>
             </li>
             <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="shop.php" id="navbarDropdownShop" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle <?php if($currentPage == 'shop.php'){echo 'active';} ?>" href="shop.php" id="navbarDropdownShop" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="fas fa-store"></i> Shop
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownShop">
-                <li><a class="dropdown-item" href="shop.php">All Items</a></li>
+                <li><a class="dropdown-item <?php if($currentPage == 'shop.php'){echo 'active';} ?>" href="shop.php">All Items</a></li>
                 <li><a class="dropdown-item" href="#">Gel</a></li>
                 <li><a class="dropdown-item" href="#">Soap</a></li>
             </ul>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="contact.php"><i class="fas fa-envelope"></i> Contact Us</a>
+            <a class="nav-link <?php if($currentPage == 'contact.php'){echo 'active';} ?>" href="contact.php"><i class="fas fa-envelope"></i> Contact Us</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="blog.php"><i class="fas fa-blog"></i> Blog</a>
+            <a class="nav-link <?php if($currentPage == 'blog.php'){echo 'active';} ?>" href="blog.php"><i class="fas fa-blog"></i> Blog</a>
             </li>
             <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="learn-more.php" id="navbarDropdownLearnMore" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle <?php if($currentPage == 'learn-more.php'){echo 'active';} ?>" href="learn-more.php" id="navbarDropdownLearnMore" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="fas fa-info-circle"></i> Learn More
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownLearnMore">
-                <li><a class="dropdown-item" href="learn-more.php">Benefits</a></li>
-                <li><a class="dropdown-item" href="learn-more.php">Recipes</a></li>
-                <li><a class="dropdown-item" href="learn-more.php">Nutritional Info</a></li>
-                <li><a class="dropdown-item" href="learn-more.php">How It's Made</a></li>
+                <li><a class="dropdown-item <?php if($currentPage == 'learn-more.php'){echo 'active';} ?>" href="learn-more.php">All</a></li>
+                <li><a class="dropdown-item <?php if($currentPage == 'benefits.php'){echo 'active';} ?>" href="benefits.php">Benefits</a></li>
+                <li><a class="dropdown-item <?php if($currentPage == 'recipes.php'){echo 'active';} ?>" href="recipes.php">Recipes</a></li>
+                <li><a class="dropdown-item <?php if($currentPage == 'nutritional-info.php'){echo 'active';} ?>" href="nutritional-info.php">Nutritional Info</a></li>
+                <li><a class="dropdown-item <?php if($currentPage == 'how-its-made.php'){echo 'active';} ?>" href="how-its-made.php">How It's Made</a></li>
             </ul>
             </li>
         </ul>
